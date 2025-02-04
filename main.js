a04038f3593303854db2644357efc911eea2aed1
Michelle-N25 <michelleruth2003@gmail.com>
2025-02-04 13:11:53 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//regular array
+let students = ['michelle', 'cedra', 'davis'];
+//pop- pop()
+students.pop();
+console.log(students);
+
+//push
+students.push('musa', 'sarah');
+console.log(students);
+
+//shift()
+ students.shift();
+ console.log(students);
+
+ //unshift
+ students.unshift('elijah', 'edwin');
+console.log(students);
+
+//slice
+students.slice('sarah', "edwin");
+console.log(students);
+
+// let bbcStudents = ["musa", "joan", "mosh"];
+// console.log(students);
+
+//associative arrays
+ let items = {
+    "fruit":'apple',
+    "vegetable":'nakati',
+    "otheritem":'mango',
+ };
+ console.log(items.fruit);
